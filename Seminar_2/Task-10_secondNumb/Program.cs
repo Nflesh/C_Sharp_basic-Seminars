ac3348e5ae833dc8d5f326cd01543dd6031188f0
Faso <stepan.pk2@gmail.com>
2023-02-01 11:37:14 +0400

    Добавление 10 задачи. Нахождение 2й цифры

---
+++ Seminar_2/Task-10_secondNumb/Program.cs
@@ -0,0 +1,11 @@
+﻿ // найти 3ю цифру числа
+
+int number;
+int secondNumber;
+
+Console.Write("Введите трехзначное число: ");
+number = Convert.ToInt32(Console.ReadLine());
+
+secondNumber = (number % 100 - number % 10) / 10 ;
+
+Console.WriteLine($"2я цифра числа {number} = {secondNumber}");
