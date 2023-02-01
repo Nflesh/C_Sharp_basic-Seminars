3aa3bf612b5bae5ff0b138c56cb609c13e3c6436
Faso <stepan.pk2@gmail.com>
2023-02-01 12:33:58 +0400

    Добавлена 13 задача. НАхождение 3й цифры

---
+++ Seminar_2/Task-13_thirdNumb/Program.cs
@@ -0,0 +1,28 @@
+﻿// нахождение 3го числа
+
+int number;
+int thirdNumber;
+//int checkNumberSize  = 0;
+
+Console.Write("Введите число: ");
+number = Convert.ToInt32(Console.ReadLine());
+
+thirdNumber = number; // буферная переменная для красивого вывода в конце
+
+if( thirdNumber / 100 > 0)
+{
+    while ( thirdNumber / 100 > 10)
+    {
+        thirdNumber /= 10;
+
+    }
+    
+    thirdNumber %= 100; // для нахождения двухзначного остатка
+    thirdNumber %= 10; // для нахождения 3й цифры  
+}
+else
+{
+    Console.WriteLine("Число двухзначное. 3я цифра отсутсвует");
+}
+
+Console.WriteLine($"3я цифра числа {number} = {thirdNumber}");
