7e467d946b3db703f07b556c4837f200bc5fdd41
Faso <stepan.pk2@gmail.com>
2023-02-01 11:08:53 +0400

    Добавленj 15 задание. Дни недели

---
+++ Seminar_2/Task-15_weekDay/Program.cs
@@ -0,0 +1,25 @@
+﻿// Ввести цифру дня недели и сказать, это выходной или нет
+
+string weekDay;
+
+Console.Write("Введите цифру дня недели: ");
+weekDay = Console.ReadLine();
+
+
+switch(weekDay)
+{
+    case "1":
+    case "2":
+    case "3":
+    case "4":
+    case "5":
+        Console.WriteLine($"{weekDay}й день недели - это будний день.");
+        break;
+    case "6":
+    case "7":
+        Console.WriteLine($"{weekDay}й день недели - это выходной день.");
+        break;
+    default:
+        Console.WriteLine($"{weekDay}го дня недели не существует.");
+        break;
+}
