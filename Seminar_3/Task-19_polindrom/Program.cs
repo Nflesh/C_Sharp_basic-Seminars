638460f4034c7711b560aa557e20aa27a8f15be5
Faso <stepan.pk2@gmail.com>
2023-02-05 13:35:26 +0400

    Добавлены задания из семинара 3

---
+++ Seminar_3/Task-19_polindrom/Program.cs
@@ -0,0 +1,54 @@
+﻿// проверка на полиндром
+
+void PullInArray(int[] arr)
+{
+    //Random rand = new Random();
+    Console.WriteLine("Введите значения массива:");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = Convert.ToInt32(Console.ReadLine());
+    }
+
+}
+
+void PrintArray(int[] arr)
+{
+    int count = arr.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write(arr[i] + ", ");
+    }
+    Console.WriteLine();
+}
+
+void ChekOnPolindrom(int[] arr)
+{
+    bool isPolindrom = false;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(arr[i] == arr[arr.Length-i-1])
+        {
+            isPolindrom = true;
+        }
+    }
+    if(isPolindrom)
+    {
+        Console.WriteLine("Массив является полиндромом.");
+    }
+    else
+    {
+        Console.WriteLine("Массив не является полиндромом.");
+    }
+}
+
+Console.WriteLine("Введите размер массива: ");
+int lengthArray = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[lengthArray];
+
+PullInArray(array);
+PrintArray(array);
+ChekOnPolindrom(array);
+
+
+
+
---
+++ Seminar_3/Task-23_degreeTable/Program.cs
@@ -0,0 +1,19 @@
+﻿void CubeTable(int numb)
+{
+    int temp;
+    for (int i = 1; i <= numb; i++)
+    {
+        temp = i;
+        temp = Convert.ToInt32(Math.Pow(temp,3));
+        Console.Write(temp + " ");
+    }
+    
+}
+
+
+
+
+Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+CubeTable(number);
