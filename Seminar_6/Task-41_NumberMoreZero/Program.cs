0d887f499035d1679ead459a63f955330b5b501d
Faso <stepan.pk2@gmail.com>
2023-02-19 11:54:44 +0400

    Добавлена задача 41 и 43

---
+++ Seminar_6/Task-41_NumberMoreZero/Program.cs
@@ -0,0 +1,37 @@
+﻿/* задача 41 применять 
+var array = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+задача 43 y=k1*x+b1 y=k2*x+b2  - система уравнений k1*x+b1 = k2*x+b2 
+использовать функции
+*/
+int[] GetArray(string text)
+{
+    Console.WriteLine(text);
+    int[] array = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+    return array;
+}
+
+int CounterPositivNumber(int[] arr)
+{
+    int counter = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(arr[i] >= 0)
+        {
+            counter++;
+        }
+    }
+    
+    return counter;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.WriteLine(string.Join(" | ", arr));
+}
+
+int[] array = GetArray("Введите массив:");
+
+PrintArray(array);
+Console.WriteLine($"Число положительных чисел в массиве {CounterPositivNumber(array)}");
+
+
---
+++ Seminar_6/Task-43_TwoLinePoint/Program.cs
@@ -0,0 +1,39 @@
+﻿double GetCoefficient(string text)
+{
+    Console.WriteLine(text);    
+    return Convert.ToDouble(Console.ReadLine());
+}
+
+void PrintCoefficient(double b1,double b2,double k1,double k2)
+{
+    Console.WriteLine("Коэффициенты равны: \n"
+                + "b1 = " + b1 + " \n"
+                + "b2 = " + b2 + " \n"
+                + "k1 = " + k1 + " \n"
+                + "k2 = " + k2 + " \n");
+}
+
+void FindPoint(double b1,double b2,double k1,double k2)
+{
+    if(k1 != k2)
+    {
+        double x, y1, y2;
+        x = (-b1+b2)/(k1-k2);
+        y1 = k1 * x +b1;
+        y2 = k2 * x +b2;
+        Console.WriteLine("Координаты точки пересечения" + y1 + ", " + x + ", " + y2);
+    }
+    else
+    {
+        Console.WriteLine("Т.к. к1 и к2 равны, то прямые совпадают или параллельны");
+    }
+}
+
+double B1, B2, K1, K2;
+
+B1 = GetCoefficient("Введите коэффициент b1:");
+B2 = GetCoefficient("Введите коэффициент b2:");
+K1 = GetCoefficient("Введите коэффициент k1:");
+K2 = GetCoefficient("Введите коэффициент k2:");
+PrintCoefficient(B1,B2,K1,K2);
+FindPoint(B1,B2,K1,K2);
