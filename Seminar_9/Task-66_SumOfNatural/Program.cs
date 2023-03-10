32af8299a0f2b95e5d389c7bb959102bd7086acc
Faso <stepan.pk2@gmail.com>
2023-03-10 16:41:29 +0400

    Добавлен семинар 9

---
+++ Seminar_9/Task-66_SumOfNatural/Program.cs
@@ -0,0 +1,31 @@
+﻿int ReadFromConsole(string text)
+{
+    Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void PrintNaturalNumber(int first, int last)
+{
+    if(first <= last)
+    {
+        Console.Write(first + ", ");
+        first++;
+    
+        PrintNaturalNumber(first, last);
+    }
+}
+
+int firstNumber = ReadFromConsole("Введите первоначальное число:");
+int endNumber = ReadFromConsole("Введите конечное число:");
+
+if(firstNumber < endNumber)
+{
+    PrintNaturalNumber(firstNumber, endNumber);
+}
+else
+{
+    Console.WriteLine("Введены неправельные значения");
+}
+
+
+
---
+++ Seminar_9/Task-68_Akkerman/Program.cs
@@ -0,0 +1,27 @@
+﻿int ReadFromConsole(string text)
+{
+    Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void PrintNumber(int number)
+{
+    Console.WriteLine(number);
+}
+
+int AkermanFunction(int first, int second)
+{
+  if (first == 0)
+    return second + 1;
+  else
+    if ((first != 0) && (second == 0))
+      return AkermanFunction(first - 1, 1);
+    else
+      return AkermanFunction(first - 1, AkermanFunction(first, second - 1));
+}
+
+int firstNumber = ReadFromConsole("Введите первое число: ");
+int secondNumber = ReadFromConsole("Введите второе число: ");
+
+PrintNumber(AkermanFunction(firstNumber, secondNumber));
+
